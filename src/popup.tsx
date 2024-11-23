40eb2e0020f7610ed11dfac329735dd94bbc1668
Alan He <qianghe421@gmail.com>
2024-11-24 00:02:52 +0800

    feat(popup): 更新弹出窗口的UI和交互行为

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const Popup = () => {
@@ -11,19 +11,47 @@ const Popup = () => {
   }, []);
 
   return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-    </>
+    <div style={{
+      width: 300,
+      height: 100,
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 10,
+    }}>
+      <div>
+        Current URL: {currentURL}
+      </div>
+      <div>
+        <label>
+          Click Behavior:
+          <select defaultValue={'download'} onChange={(_) => {
+
+          }}>
+            <option value={'download'}>Download</option>
+            <option value={'copy_to_clipboard'}>Copy to Clipboard</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          Modifier Key:
+          <select defaultValue={'alt'} onChange={(_) => {
+
+          }}>
+            <option value={'alt'}>Alt/Option</option>
+            <option value={'ctrl'}>Ctrl</option>
+            <option value={''}>Off</option>
+          </select>
+        </label>
+      </div>
+    </div>
   );
 };
 
-const root = createRoot(document.getElementById("root")!);
+const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <Popup />
-  </React.StrictMode>
+    <Popup/>
+  </React.StrictMode>,
 );
